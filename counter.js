6690ae35886d6c8aabfd19d216ec462f0a8c7b03
Santhosh Raghav <santhoshraghav.work@gmail.com>
2023-11-16 14:01:02 +0000

    Update 4 files

--- counter.js
+++ counter.js
@@ -1,9 +1,14 @@
 export function setupCounter(element) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
 }
+
+const test = "hello";
+
+console.log("hi test1i kwjhckwh wknwkdlewkl wwlljwojdew");
+var val = "test1";
